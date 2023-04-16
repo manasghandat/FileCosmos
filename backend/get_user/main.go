35cd6028f8f05b502ca9d9bf4f10ee8951f005a2
Abhyuday0409 <a_singh1@cs.iitr.ac.in>
2023-04-16 17:36:32 +0530

    feat go setup

---
+++ backend/get_user/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
+
+type User struct {
+	Name  string `firestore:"name"`
+	Age   int    `firestore:"age"`
+	Email string `firestore:"email"`
+}
+
+func main() {
+	ctx := context.Background()
+	opt := option.WithCredentialsFile("../../backend/location-based-file-sharing-firebase-adminsdk-tzvgx-fb430b4173.json")
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+
+	// Access Firestore database
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalf("error initializing firestore client: %v\n", err)
+	}
+	defer client.Close()
+
+	// Get a reference to the "users" collection
+	users := client.Collection("users")
+
+	// Get the document with ID "alice"
+	doc, err := users.Doc("alice").Get(ctx)
+	if err != nil {
+		log.Fatalf("error getting document: %v", err)
+	}
+
+	// Deserialize the document data into a User struct
+	var user User
+	err = doc.DataTo(&user)
+	if err != nil {
+		log.Fatalf("error deserializing document data: %v", err)
+	}
+
+	log.Printf("name: %s, age: %d, email: %s", user.Name, user.Age, user.Email)
+}
---
+++ backend/upload_file/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"cloud.google.com/go/storage"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	ctx := context.Background()
+
+	opt := option.WithCredentialsFile("../../backend/location-based-file-sharing-firebase-adminsdk-tzvgx-fb430b4173.json")
+
+	// Access Cloud Storage
+	client, err := storage.NewClient(ctx, opt)
+	if err != nil {
+		log.Fatalf("error initializing cloud storage client: %v", err)
+	}
+	defer client.Close()
+
+	// Get a reference to the default bucket
+	bucketName := "location-based-file-sharing.appspot.com"
+	bucket := client.Bucket(bucketName)
+
+	// Read file contents
+	filePath := "/home/abhyuday/Desktop/ooad/Input.txt"
+	fileContents, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatalf("error reading file: %v", err)
+	}
+
+	// Upload file to bucket with specified name
+	objectName := "Input.txt"
+	wc := bucket.Object(objectName).NewWriter(ctx)
+	if _, err = wc.Write(fileContents); err != nil {
+		log.Fatalf("error writing file to bucket: %v", err)
+	}
+	if err := wc.Close(); err != nil {
+		log.Fatalf("error closing writer: %v", err)
+	}
+
+	log.Printf("file %s uploaded to bucket %s", objectName, bucketName)
+}
