042240d46708dac1884696cf7b4e3e3d0845cfb8
drcode03 <ijatingrover@gmail.com>
2023-04-17 03:40:36 +0530

    listview

--- file_cosmos/lib/screens/receive_screen/receive_screen.dart
+++ file_cosmos/lib/screens/receive_screen/receive_screen.dart
@@ -1,208 +1,135 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../models/file_model.dart';
+// import 'package:../../models/file_model.dart';
+
 class ReceiveScreen extends StatelessWidget {
   const ReceiveScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back,color:Colors.purple.shade700 ,),
-            
-            onPressed: () {},
-          ),
+    List<MyFile> Lis = [
+      MyFile(
+          id: "id",
+          name: "Name",
+          location: "location",
+          coordinates: "coordinates")
+    ];
 
-          backgroundColor: Colors.white,
-          elevation: 0,
-          title: Text(
-            'File Transfer',
-            style: GoogleFonts.lato(
-              color: Colors.purple,
-              fontWeight: FontWeight.bold,
-              fontSize: 24,
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.purple.shade700,
+          ),
+          onPressed: () {},
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          'File Transfer',
+          style: GoogleFonts.lato(
+            color: Colors.purple,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 50),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 80),
+                  width: 250,
+                  height: 250,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: const Color.fromARGB(255, 237, 144, 161),
+                      width: 3,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-          centerTitle: true,
+            const SizedBox(height: 56),
+            Stack(
+              alignment: Alignment.topLeft,
+              children: [
+                const Text(
+                  'Recieved Files',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 7, 133, 236),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    // alignment: LexicalFocusOrder,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: 100,
+              child: ListView.builder(
+                  padding: const EdgeInsets.all(10),
+                  itemCount: Lis.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      height: 60,
+                      // width: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          color: const Color.fromARGB(255, 5, 157, 99),
+                          width: 2,
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.all(5),
+                            child: Icon(
+                              Icons.insert_drive_file,
+                              color: Colors.purple,
+                              size: 35,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(8),
+                            child: Column(children: [
+                              Text(
+                                Lis[index].name,
+                                style: GoogleFonts.lato(
+                                  color: const Color.fromARGB(255, 0, 0, 0),
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 18,
+                                ),
+                              ),
+                              Text(
+                                Lis[index].location,
+                                style: GoogleFonts.lato(
+                                  color: Color.fromARGB(125, 18, 17, 17),
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ]),
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+            )
+          ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const SizedBox(height: 50),
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.only(left: 80),
-                    width: 250,
-                    height: 250,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      
-                      border: Border.all(
-                        
-                        color: const Color.fromARGB(255, 237, 144, 161),
-                        width: 3,
-                      ),
-                    ),
-                  ),
-                  
-                ],
-              ),
-              const SizedBox(height: 56),
-              Stack(
-                alignment: Alignment.topLeft,
-                children: [
-                  // Container(
-                  //   padding: EdgeInsets.only(left: 80),
-                  //   width: 60,
-                  //   height: 60,
-                  //   decoration: BoxDecoration(
-                  //     shape: BoxShape.rectangle,
-                  //     border: Border.all(
-                        
-                  //       color: Color.fromARGB(185, 154, 65, 65),
-                  //       width: 2,
-                  //     ),
-                  //   ),
-                  // ),
-                  const Text(
-                    
-                    'File Details',
-                    style: TextStyle(
-                      
-                      color: Color.fromARGB(255, 7, 133, 236),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      // alignment: LexicalFocusOrder,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              Container(
-                height: 40,
-                // width: 40,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(
-                    color: const Color.fromARGB(255, 5, 157, 99),
-                    width: 2,
-                  ),
-                ),
-                child: Row(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.all(8),
-                      child: Icon(
-                        Icons.insert_drive_file,
-                        color: Colors.purple,
-                      ),
-                    ),
-                    Text(
-                      'fileName 1',
-                      style: GoogleFonts.lato(
-                        color: const Color.fromARGB(255, 0, 0, 0),
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 16),
-              Container(
-                height: 40,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(
-                    color: const Color.fromARGB(255, 5, 157, 99),
-                    width: 2,
-                  ),
-                ),
-                child: Row(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.all(8),
-                      child: Icon(
-                        Icons.insert_drive_file,
-                        color: Colors.purple,
-                      ),
-                    ),
-                    Text(
-                      'fileName 2',
-                      style: GoogleFonts.lato(
-                        color: const Color.fromARGB(255, 0, 0, 0),
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 16),
-              Container(
-                height: 40,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(
-                    color: const Color.fromARGB(255, 5, 157, 99),
-                    width: 2,
-                  ),
-                ),
-                child: Row(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.all(8),
-                      child: Icon(
-                        Icons.insert_drive_file,
-                        color: Colors.purple,
-                      ),
-                    ),
-                    Text(
-                      'fileName 3',
-                      style: GoogleFonts.lato(
-                        color: const Color.fromARGB(255, 0, 0, 0),
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 16),
-              Container(
-                height: 40,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(
-                    color: const Color.fromARGB(255, 5, 157, 99),
-                    width: 2,
-                  ),
-                ),
-                child: Row(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.all(8),
-                      child: Icon(
-                        Icons.insert_drive_file,
-                        color: Colors.purple,
-                      ),
-                    ),
-                    Text(
-                      'fileName 4',
-                      style: GoogleFonts.lato(
-                        color: const Color.fromARGB(255, 0, 0, 0),
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const Spacer(),
-            ],
-          ),
-        ),
-      );
+      ),
+    );
   }
 }
