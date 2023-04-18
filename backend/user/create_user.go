102cb6bade050bb887fcf1275734b3b5974d10bd
manasghandat <ghandatmanas@gmail.com>
2023-04-18 16:12:54 +0530

    Fix conflicts

--- backend/user/create_user.go
+++ backend/user/create_user.go
@@ -19,19 +19,3 @@ func CreateUser(ctx context.Context, client *firestore.Client, user models.User)
 	return nil
 }
 
-func GetUser(ctx context.Context, client *firestore.Client, name string) (models.User, error) {
-	var user models.User
-
-	// Retrieve the user from the "users" collection in Firestore
-	doc, err := client.Collection("users").Doc(name).Get(ctx)
-	if err != nil {
-		return models.User{}, err
-	}
-
-	// Unmarshal the document data into a User struct
-	if err := doc.DataTo(&user); err != nil {
-		return models.User{}, err
-	}
-
-	return user, nil
-}
